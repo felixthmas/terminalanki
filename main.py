bfb02ad523189895ef385a114bfca0ed63b23264
Felix <73547199+felixthmas@users.noreply.github.com>
2020-11-15 09:10:09 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+version='0.1'
+import time
+print ('-'*40)
+time.sleep(.3)
+print('WELCOME TO MYANKI ' + version)
+time.sleep(1)
+print('An open-source terminal-based anki version')
+time.sleep(.3)
+print('github.com/felixthmas/terminalanki')
+time.sleep(.5)
+print ('-'*40)
+
